de81f544366394b6bf71ff6945adcbdc8cb63024
FMRodrigues97 <63131709+FMRodrigues97@users.noreply.github.com>
2022-04-08 11:33:48 -0300

    Add files via upload

---
+++ ex2/semana2.m
@@ -0,0 +1,16 @@
+clear, clc;
+
+% Load Data
+% The first two columns contain the exam scores and the third column contains the label.
+data = load('ex2data1.txt');
+X = data(:, [1, 2]); 
+y = data(:, 3);
+
+%  Setup the data matrix appropriately
+[m, n] = size(X);
+
+% Add intercept term to X
+X = [ones(m, 1) X];
+
+% Initialize the fitting parameters
+initial_theta = zeros(n + 1, 1);
